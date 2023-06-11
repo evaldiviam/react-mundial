6bad081b77903d7da0bd39e6d0dbf017796857ed
evaldiviam <evaldivia@iesemilidarder.com>
2023-06-11 12:33:45 +0200

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # CLON MUNDIAL QATAR RESULTS 
 
 ## Backend
+```
 >cd backend
 >npm start 
+```
 
 ## Frontend
+```
+>npm start 
+```
--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,14 @@ app.use('/api/images', express.static('uploads'));
 
 // Habilitar cors
 app.use(cors());
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 // Rutas
 app.use("/api", gamesRoutes);
